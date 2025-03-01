ffba9373f689a200753ce3cadd57d895b78cd122
Viktor Petruk <damnnn261@gmail.com>
2025-03-02 01:48:47 +0200

    create .env for supabase sensetive data

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://adhvzazqdxgavjwwrygp.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkaHZ6YXpxZHhnYXZqd3dyeWdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NzA4MDUsImV4cCI6MjA1NjQ0NjgwNX0.nMWjZHTebWE0Rl_ihH93Y07XNaDLW7aU4io4kVSm9QU'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
+
+console.log(import.meta.env.VITE_SUPABASE_URL)
