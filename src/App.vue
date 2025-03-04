ee58b4f3f362d579e0dfd0980b38d82fd9960668
Viktor Petruk <damnnn261@gmail.com>
2025-03-04 06:57:50 +0200

    utilize shadcn input and dropdown, set up shadcn properly

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex items-center justify-between px-6">
+    <form class="w-full max-w-96">
+      <Input placeholder="Search..." type="text" class="w-full bg-background pl-8" />
+    </form>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
   <main>
     <RouterView />
   </main>
