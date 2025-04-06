76d88d1ef410ccc49db9c947df8bfc0f64a4486b
Viktor Petruk <damnnn261@gmail.com>
2025-04-06 12:36:57 +0300

    Use Vue.js Suspense Component to Handle Async Dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
+import { set } from '@vueuse/core'
+
+const MocComponent = defineComponent(async () => {
+  const greeting = ref('Hello')
+
+  await new Promise((resolve) => {
+    greeting.value = 'Hello from the Database'
+    setTimeout(() => {
+      resolve(true)
+    }, 2000)
+  })
+  return () => h('p', greeting.value)
+})
 </script>
 
 <template>
   <AuthLayout>
-    <RouterView />
+    <Suspense>
+      <MocComponent />
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
+    <!-- <RouterView /> -->
   </AuthLayout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
